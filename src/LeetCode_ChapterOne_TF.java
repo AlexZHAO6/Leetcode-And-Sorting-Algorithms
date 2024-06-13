59bcab7da6173c9e9a97ffb798a8041f54f8c61b
alexzhao <alexzhao562@gmail.com>
2024-06-13 15:22:43 +0800

    binary tree

--- src/LeetCode_ChapterOne_TF.java
+++ src/LeetCode_ChapterOne_TF.java
@@ -515,6 +515,82 @@ class BinaryTree_ChapterOne{
         }
     }
 
+    public TreeNode invertTree(TreeNode root) {
+        //核心在于你要给递归函数一个合适的定义，然后用函数的定义来解释你的代码；如果你的逻辑成功自恰，那么说明你这个算法是正确的。
+        if(root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public Node connect(Node root) {
+        if (root == null) return null;
+        // 遍历「三叉树」，连接相邻节点
+        traverse(root.left, root.right);
+        return root;
+    }
+    // 三叉树遍历框架
+    void traverse(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        /**** 前序位置 ****/
+        // 将传入的两个节点穿起来
+        node1.next = node2;
+
+        // 连接相同父节点的两个子节点
+        traverse(node1.left, node1.right);
+        traverse(node2.left, node2.right);
+        // 连接跨越父节点的两个子节点
+        traverse(node1.right, node2.left);
+    }
+
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+
+        // 利用定义，把左右子树拉平
+        flatten(root.left);
+        flatten(root.right);
+
+        /**** 后序遍历位置 ****/
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while(p.right != null) p = p.right;
+        p.right = right;
+    }
+
 
 }
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
 
