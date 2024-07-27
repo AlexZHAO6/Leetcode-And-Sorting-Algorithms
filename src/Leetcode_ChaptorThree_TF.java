776e319dd232d19ba45533c1f22c9d04eb59a721
alex-pc <alexzhao562@gmail.com>
2024-07-27 15:25:01 +0800

    backtrack

--- src/Leetcode_ChaptorThree_TF.java
+++ src/Leetcode_ChaptorThree_TF.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Leetcode_ChaptorThree_TF {
     public static void main(String[] args) {
 
@@ -5,6 +6,7 @@ public class Leetcode_ChaptorThree_TF {
 }
 
 class BackTrackAlgorithm{
+    boolean solveSudoku = false;
     public int numIslands(char[][] grid) {
         int res = 0;
         int m = grid.length, n = grid[0].length;
@@ -106,4 +108,133 @@ class BackTrackAlgorithm{
                 dfs_max(grid, i - 1, j) +
                 dfs_max(grid, i, j - 1) + 1;
     }
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length, n = grid1[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid1[i][j] == 0 && grid2[i][j] == 1) {
+                    // 这个岛屿肯定不是子岛，淹掉
+                    dfs_countSubIslands(grid2, i, j);
+                }
+            }
+        }
+        // 现在 grid2 中剩下的岛屿都是子岛，计算岛屿数量
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    res++;
+                    dfs_countSubIslands(grid2, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    // 从 (i, j) 开始，将与之相邻的陆地都变成海水
+    void dfs_countSubIslands(int[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
+        }
+        if (grid[i][j] == 0) {
+            return;
+        }
+
+        grid[i][j] = 0;
+        dfs_countSubIslands(grid, i + 1, j);
+        dfs_countSubIslands(grid, i, j + 1);
+        dfs_countSubIslands(grid, i - 1, j);
+        dfs_countSubIslands(grid, i, j - 1);
+    }
+
+    public int numDistinctIslands(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        // 记录所有岛屿的序列化结果
+        HashSet<String> islands = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    // 淹掉这个岛屿，同时存储岛屿的序列化结果
+                    StringBuilder sb = new StringBuilder();
+                    // 初始的方向可以随便写，不影响正确性
+                    dfs_numDistinctIslands(grid, i, j, sb, 666);
+                    islands.add(sb.toString());
+
+                }
+            }
+        }
+        // 不相同的岛屿数量
+        return islands.size();
+    }
+    void dfs_numDistinctIslands(int[][] grid, int i, int j, StringBuilder sb, int dir) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n
+                || grid[i][j] == 0) {
+            return;
+        }
+        // 前序遍历位置：进入 (i, j)
+        grid[i][j] = 0;
+        sb.append(dir).append(',');
+
+        dfs_numDistinctIslands(grid, i - 1, j, sb, 1); // 上
+        dfs_numDistinctIslands(grid, i + 1, j, sb, 2); // 下
+        dfs_numDistinctIslands(grid, i, j - 1, sb, 3); // 左
+        dfs_numDistinctIslands(grid, i, j + 1, sb, 4); // 右
+
+        // 后序遍历位置：离开 (i, j)
+        sb.append(-dir).append(',');
+    }
+
+    public void solveSudoku(char[][] board) {
+        backtrack_solveSudoku(board, 0, 0);
+    }
+    void backtrack_solveSudoku(char[][] board, int i, int j) {
+        int m = 9, n = 9;
+        if (j == n) {
+            // 穷举到最后一列的话就换到下一行重新开始。
+            backtrack_solveSudoku(board, i + 1, 0);
+            return;
+        }
+        if (i == m) {
+            // 找到一个可行解，触发 base case
+            solveSudoku = true;
+            return;
+        }
+
+        if (board[i][j] != '.') {
+            // 如果有预设数字，不用我们穷举
+            backtrack_solveSudoku(board, i, j + 1);
+            return;
+        }
+
+        for (char ch = '1'; ch <= '9'; ch++) {
+            // 如果遇到不合法的数字，就跳过
+            if (!isValid(board, i, j, ch))
+                continue;
+
+            board[i][j] = ch;
+
+            // 如果找到一个可行解，立即结束
+            backtrack_solveSudoku(board, i, j + 1);
+            if(solveSudoku) return;
+            board[i][j] = '.';
+        }
+    }
+    // 判断 board[i][j] 是否可以填入 n
+    boolean isValid(char[][] board, int r, int c, char n) {
+        for (int i = 0; i < 9; i++) {
+            // 判断行是否存在重复
+            if (board[r][i] == n) return false;
+            // 判断列是否存在重复
+            if (board[i][c] == n) return false;
+            // 判断 3 x 3 方框是否存在重复
+            if (board[(r/3)*3 + i/3][(c/3)*3 + i%3] == n)
+                return false;
+        }
+        return true;
+    }
 }
+
+
