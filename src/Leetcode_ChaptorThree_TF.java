0592fa658bb1ac185ab330c971dbc99a1ebf1583
alexzhao <alexzhao562@gmail.com>
2024-07-29 14:15:47 +0800

    go

--- src/Leetcode_ChaptorThree_TF.java
+++ src/Leetcode_ChaptorThree_TF.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Leetcode_ChaptorThree_TF {
     public static void main(String[] args) {
 
@@ -341,3 +342,126 @@ class BackTrackAlgorithm{
 }
 
 
+
+class BFS_Algorithm{
+    public int openLock(String[] deadends, String target) {
+        // 记录需要跳过的死亡密码
+        Set<String> deads = new HashSet<>();
+        for (String s : deadends) deads.add(s);
+        // 记录已经穷举过的密码，防止走回头路
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        // 从起点开始启动广度优先搜索
+
+        int step = 0;
+        q.offer("0000");
+        visited.add("0000");
+
+        while(!q.isEmpty()){
+            int sz = q.size();
+            /* 将当前队列中的所有节点向周围扩散  !!! significant!!*/
+            for(int i = 0; i < sz; i++){
+                String cur = q.poll();
+                if(deads.contains(cur)) continue;
+                if(cur.equals(target)) return step;
+
+                for(int j = 0; j < 4; j++){
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up)) {
+                        q.offer(up);
+                        visited.add(up);
+                    }
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down)) {
+                        q.offer(down);
+                        visited.add(down);
+                    }
+                }
+            }
+            step++;
+
+        }
+        return -1;
+    }
+    String plusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '9')
+            ch[j] = '0';
+        else
+            ch[j] += 1;
+        return new String(ch);
+    }
+    // 将 s[i] 向下拨动一次
+    String minusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '0')
+            ch[j] = '9';
+        else
+            ch[j] -= 1;
+        return new String(ch);
+    }
+
+    public int slidingPuzzle(int[][] board) {
+        int m = 2, n = 3;
+        StringBuilder sb = new StringBuilder();
+        String target = "123450";
+        // 将 2x3 的数组转化成字符串作为 BFS 的起点
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(board[i][j]);
+            }
+        }
+        String start = sb.toString();
+
+        // 记录一维字符串的相邻索引
+        int[][] neighbor = new int[][]{
+
+                {1, 3},
+                {0, 4, 2},
+                {1, 5},
+                {0, 4},
+                {3, 1, 5},
+                {4, 2}
+        };
+
+        /******* BFS 算法框架开始 *******/
+        Queue<String> q = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+        // 从起点开始 BFS 搜索
+        q.offer(start);
+        visited.add(start);
+
+        int step = 0;
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                String cur = q.poll();
+                // 判断是否达到目标局面
+                if (target.equals(cur)) {
+                    return step;
+                }
+                // 找到数字 0 的索引
+                int idx = 0;
+                for (; cur.charAt(idx) != '0'; idx++) ;
+                // 将数字 0 和相邻的数字交换位置
+                for (int adj : neighbor[idx]) {
+                    String new_board = swap(cur.toCharArray(), adj, idx);
+                    // 防止走回头路
+                    if (!visited.contains(new_board)) {
+                        q.offer(new_board);
+                        visited.add(new_board);
+                    }
+                }
+            }
+            step++;
+        }
+        /******* BFS 算法框架结束 *******/
+        return -1;
+    }
+    private String swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+        return new String(chars);
+    }
+}
