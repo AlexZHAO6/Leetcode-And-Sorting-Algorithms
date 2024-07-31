fc53e333d5e79a4c2aec319e22ed3053feb06aa1
alexzhao <alexzhao562@gmail.com>
2024-07-31 17:38:29 +0800

    go

--- src/LeetCode_ChapterTwo_TF.java
+++ src/LeetCode_ChapterTwo_TF.java
@@ -94,5 +94,88 @@ class DP_BASIC{
         }
         return dp[s.length()];
     }
+
+    public int minDistance(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        // 定义：s1[0..i] 和 s2[0..j] 的最小编辑距离是 dp[i+1][j+1]
+        int[][] dp = new int[m + 1][n + 1];
+        // base case
+        for (int i = 1; i <= m; i++)
+            dp[i][0] = i;
+        for (int j = 1; j <= n; j++)
+            dp[0][j] = j;
+        // 自底向上求解
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(
+                            dp[i - 1][j] + 1,
+
+                            dp[i][j - 1] + 1,
+
+                            dp[i - 1][j - 1] + 1
+
+                    );
+                }
+            }
+        }
+        // 储存着整个 s1 和 s2 的最小编辑距离
+        return dp[m][n];
+    }
+
+    int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        if(len == 0) return 0;
+        int[] dp = new int[len];
+
+        dp[0] = nums[0];
+        int res = dp[0];
+        for(int i = 1; i < len; i++){
+            dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        // 定义：s1[0..i-1] 和 s2[0..j-1] 的 lcs 长度为 dp[i][j]
+        // 目标：s1[0..m-1] 和 s2[0..n-1] 的 lcs 长度，即 dp[m][n]
+        // base case: dp[0][..] = dp[..][0] = 0
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // 现在 i 和 j 从 1 开始，所以要减一
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    // s1[i-1] 和 s2[j-1] 必然在 lcs 中
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    // s1[i-1] 和 s2[j-1] 至少有一个不在 lcs 中
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public int minDistance_2(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        // 复用前文计算 lcs 长度的函数
+        int lcs = longestCommonSubsequence(word1, word2);
+        return m - lcs + n - lcs;
+    }
+
+    public int minimumDeleteSum(String s1, String s2) {
+
+    }
 }
 
