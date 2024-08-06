cf2fb586d38c0fcdff540577bb65610dca045e37
alex-pc <alexzhao562@gmail.com>
2024-08-06 22:38:31 +0800

    go

--- src/LeetCode_ChapterTwo_TF.java
+++ src/LeetCode_ChapterTwo_TF.java
@@ -420,8 +420,27 @@ class DP_BASIC{
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
         for (int i = 1; i < n; i++) {
+            //如果 k 为正无穷，那么就可以认为 k 和 k - 1 是一样的。可以这样改写框架：
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
             dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+
+    public int maxProfit_3(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        if(n < 2) return 0;
+        dp[1][0] = Math.max(dp[0][0], dp[0][1] + prices[1]);
+        dp[1][1] = Math.max(dp[0][1], -prices[1]);
+        for (int i = 2; i < n; i++) {
+            //每次 sell 之后要等一天才能继续交易，只要把这个特点融入上一题的状态转移方程即可
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            //i-2! not i-1
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0] - prices[i]);
         }
         return dp[n - 1][0];
     }
